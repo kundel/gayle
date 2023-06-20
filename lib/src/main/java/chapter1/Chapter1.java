f1e309051c6a7714bc518531b4800fdeddaebbf2
Darrell Kundel <darrell@demiflat.com>
2023-06-20 16:04:16 -0700

    1_1a, 1_2

--- lib/src/main/java/chapter1/Chapter1.java
+++ lib/src/main/java/chapter1/Chapter1.java
@@ -1,16 +1,18 @@
 package chapter1;
 
+import java.util.BitSet;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Chapter1 {
   public Chapter1() {}
 
-  public static boolean isUnique(String input) {
-    boolean hasDupe = false;
-    HashMap<Integer, Integer> map = new HashMap<>();
+  public static boolean isUnique_1(String input) {
     if (input == null) {
       throw new IllegalArgumentException("empty input string");
     }
+    boolean hasDupe = false;
+    HashMap<Integer, Integer> map = new HashMap<>();
     final int length = input.length();
     for (int offset = 0; offset < length; ) {
       final int codepoint = input.codePointAt(offset);
@@ -22,5 +24,49 @@ public class Chapter1 {
       offset += Character.charCount(codepoint);
     }
     return !hasDupe;
+  }
+
+  public static boolean isUnique_2(String input) {
+    if (input == null) {
+      throw new IllegalArgumentException("empty input string");
+    }
+    boolean hasDupe = false;
+    BitSet bitSet = new BitSet(32);
+    final int length = input.length();
+    for (int offset = 0; offset < length; ) {
+      final int codepoint = input.codePointAt(offset);
+      if (bitSet.get(codepoint)) hasDupe = true;
+      bitSet.set(codepoint);
+      offset += Character.charCount(codepoint);
+    }
+    return !hasDupe;
+  }
+
+  public static boolean isPermutation(String one, String two) {
+    if (one == null || two == null) {
+      throw new IllegalArgumentException("empty input string");
+    }
+    Map<Integer, Integer> oneIdx = index(one);
+    Map<Integer, Integer> twoIdx = index(two);
+    for (Map.Entry<Integer, Integer> e : twoIdx.entrySet()) {
+      int key = e.getKey();
+      int value = e.getValue();
+      if (!oneIdx.containsKey(key) || !oneIdx.get(key).equals(value)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static Map<Integer, Integer> index(String input) {
+    HashMap<Integer, Integer> idx = new HashMap<>();
+    final int length = input.length();
+    for (int offset = 0; offset < length; ) {
+      final int codepoint = input.codePointAt(offset);
+      int count = idx.getOrDefault(codepoint, 0);
+      idx.put(codepoint, ++count);
+      offset += Character.charCount(codepoint);
+    }
+    return idx;
   }
 }
--- lib/src/test/java/chapter1/Chapter1Test.java
+++ lib/src/test/java/chapter1/Chapter1Test.java
@@ -3,10 +3,21 @@ package chapter1;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Chapter1Test {
     @Test
     public void test1_1() {
-        Assertions.assertTrue(Chapter1.isUnique("你好，世界！"));
-        Assertions.assertFalse(Chapter1.isUnique("你好，世界世界！"));
+        assertTrue(Chapter1.isUnique_1("你好，世界！"));
+        assertFalse(Chapter1.isUnique_1("你好，世界世界！"));
+        assertTrue(Chapter1.isUnique_2("你好，世界！"));
+        assertFalse(Chapter1.isUnique_2("你好，世界世界！"));
+    }
+
+    @Test
+    public void test1_2() {
+        assertTrue(Chapter1.isPermutation("abcded", "dedcab"));
+        assertFalse(Chapter1.isPermutation("dddde", "eeeed"));
     }
 }
